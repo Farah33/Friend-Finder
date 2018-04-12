4f505726a708b7868f30369b853e6636ba7f58aa
khadija <abdix129@umn.edu>
2018-04-12 05:38:32 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,190 @@
+var friends = [
+
+    {
+        "name": "Zain Asher",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8isEzotAox9c0gqusg39KvtGFXElAKfYYHmtDwuPCyeW3Tb03",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+
+    },
+
+    {
+        "name": "Amanpour",
+        "photo": "http://i2.cdn.turner.com/cnn/2014/images/10/23/amanpour.christiane.jpg",
+        "scores": [
+            2,
+            4,
+            4,
+            7,
+            8,
+            7,
+            3,
+            1,
+            7,
+            6
+        ]
+
+
+    },
+    {
+        "name": "Putin with Trump",
+        "photo": "https://cdn.cnn.com/cnnnext/dam/assets/170803091253-02-trump-putin-0707.jpg",
+        "scores": [
+            4,
+            5,
+            6,
+            8,
+            9,
+            3,
+            4,
+            5,
+            8,
+            5
+        ]
+
+
+    },
+    {
+        "name": "Trump",
+        "photo": "https://i.ytimg.com/vi/9bC0pZCPPAE/maxresdefault.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            7,
+            7,
+            5,
+            4,
+            5,
+            8,
+            6
+        ]
+
+
+    },
+    {
+        "name": "C.Ronaldo",
+        "photo": "https://i.ytimg.com/vi/CjVSR_amufI/hqdefault.jpg",
+        "scores": [
+            4,
+            3,
+            6,
+            8,
+            7,
+            5,
+            0,
+            1,
+            3,
+            9
+        ]
+
+
+
+    },
+    {
+        "name": "Obama",
+        "photo": "https://media.newyorker.com/photos/59097c05019dfc3494ea37eb/4:3/w_728,c_limit/161128_r29100_rd.jpg",
+        "scores": [
+            1,
+            4,
+            7,
+            9,
+            4,
+            2,
+            1,
+            8,
+            6,
+            0
+        ]
+
+
+    },
+    {
+        "name": "Mandela",
+        "photo": "https://s.abcnews.com/images/International/OBIT_NelsonMandela_1918_2013_131205_16x9_992.jpg",
+        "scores": [
+            7,
+            4,
+            0,
+            7,
+            1,
+            1,
+            3,
+            4,
+            7,
+            2
+        ]
+
+
+    },
+    {
+        "name": "Mohamed Ali",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/8/89/Muhammad_Ali_NYWTS.jpg",
+        "scores": [
+            9,
+            8,
+            5,
+            7,
+            8,
+            1,
+            4,
+            8,
+            9,
+            6
+        ]
+
+
+    },
+    {
+        "name": "Gandhi",
+        "photo": "http://1zl13gzmcsu3l9yq032yyf51-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/inspiring-Gandhi-quotes-Happiness.jpg",
+        "scores": [
+            1,
+            4,
+            6,
+            7,
+            9,
+            7,
+            2,
+            1,
+            7,
+            6
+        ]
+
+
+    },
+    {
+        "name": "George Washington",
+        "photo": "http://cf.mp-cdn.net/9b/3e/174f219aeae712962f1a69f9b5c6-was-george-washington-a-good-president.jpg",
+        "scores": [
+            2,
+            4,
+            4,
+            7,
+            8,
+            7,
+            4,
+            1,
+            9,
+            6
+        ]
+
+
+    },
+
+
+];
+
+//export to the server
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,49 @@
+var friendsList = require('../data/friends');
+var path = require('path');
+
+//ROUTS
+module.exports = function(app) {
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
+
+    //API POST Request-handles when user submits a form & thus submits data to the server.
+    app.post('/api/friends', function(req, res) {
+        var characters = {
+            name: "",
+            image: "",
+            matchDifference: 1000
+        };
+        var usrData = req.body;
+        var usrName = usrData.name;
+        var usrImage = usrData.image;
+        var usrScores = usrData.scores;
+
+        var totalDifference = 0;
+
+        //loop through the friendslist data array of objects to get each friends scores
+        for (var i = 0; i < [friendsList].length - 1; i++) {
+            console.log(friendsList[i].name);
+            totalDifference = 0;
+
+            //loop through that friends score and the users score and calculate the 
+            // absolute difference between the two and push that to the total difference variable set above
+            for (var j = 0; j < 10; j++) {
+                // We calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friendsList[i].scores[j]));
+                // If the sum of differences is less then the differences of the current "best match"
+                if (totalDifference <= characters.friendDifference) {
+
+                    // Reset the bestMatch to be the new friend. 
+                    characters.name = friendsList[i].name;
+                    characters.photo = friendsList[i].photo;
+                    characters.matchDifference = totalDifference;
+                }
+            }
+        }
+
+        friendsList.push(usrData);
+
+        res.json(characters);
+    });
+};
